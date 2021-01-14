99cd23b32d627afea6847471f8a2da10f9808e86
Lawrence <Ldorianchen@gmail.com>
2021-01-14 00:27:01 -0500

    delete failed experiment

--- experimental/RunnableBot.java
+++
@@ -1,8 +0,0 @@
-package experimental;
-
-import battlecode.common.GameActionException;
-
-public interface RunnableBot {
-    public void init() throws GameActionException;
-    public void turn() throws GameActionException;
-}

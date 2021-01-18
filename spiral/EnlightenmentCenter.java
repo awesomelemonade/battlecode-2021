fbf85648465c85192d17dc720d2086d2c5905ba1
Lawrence <Ldorianchen@gmail.com>
2021-01-18 11:46:11 -0500

    experiment with early build order and p defense

---
+++ spiral/EnlightenmentCenter.java
@@ -0,0 +1,297 @@
+package spiral;
+
+import battlecode.common.*;
+import spiral.util.Cache;
+import spiral.util.CentralCommunication;
+import spiral.util.Constants;
+import spiral.util.LambdaUtil;
+import spiral.util.MapInfo;
+import spiral.util.SlandererBuild;
+import spiral.util.Util;
+
+import java.util.Comparator;
+
+public strictfp class EnlightenmentCenter implements RunnableBot {
+    private static RobotController rc;
+
+    private static int slandererCount = 0;
+    private static int muckrakerCount = 0;
+    private static int politicianCount = 0;
+    private static int eM = 0, eS = 0, eP = 0;
+    private static int lastInfluence = 0;
+    private static int perTurnProfit = 0;
+    private static int turnsSinceSelfEmpowerer = 0;
+    private static boolean initialEC;
+
+    private static MapLocation enemyDirection;
+
+    public EnlightenmentCenter(RobotController rc) {
+        EnlightenmentCenter.rc = rc;
+    }
+
+    @Override
+    public void init() throws GameActionException {
+        enemyDirection = rc.getLocation();
+        lastInfluence = rc.getInfluence();
+        initialEC = rc.getRoundNum() == 1;
+    }
+
+    public void preTurn() {
+        turnsSinceSelfEmpowerer++;
+        perTurnProfit = rc.getInfluence() - lastInfluence;
+        Util.println("Profit = " + perTurnProfit);
+    }
+
+    public void postTurn() {
+        lastInfluence = rc.getInfluence();
+    }
+
+    @Override
+    public void turn() throws GameActionException {
+        preTurn();
+        if (CentralCommunication.nearestEnemy != null) {
+            Direction directionToNearestEnemy = Cache.MY_LOCATION.directionTo(CentralCommunication.nearestEnemy);
+            enemyDirection = enemyDirection.add(directionToNearestEnemy);
+        }
+        int saveAmount = getSaveAmount();
+        Util.println("EC Turn: save=" + saveAmount + ", buff=" + rc.getEmpowerFactor(Constants.ALLY_TEAM, 0));
+        int influenceLeft = rc.getInfluence() - saveAmount - 1;
+        buildUnit(influenceLeft);
+        influenceLeft = rc.getInfluence() - saveAmount - 1;
+        bid(influenceLeft);
+        postTurn();
+    }
+
+    public static void buildUnit(int influence) {
+        if (rc.isReady()) {
+            if (reactBuild(influence)) return;
+            int unitsBuilt = slandererCount + politicianCount + muckrakerCount;
+            if (initialEC && unitsBuilt < 30) {
+                switch (unitsBuilt % 4) {
+                    case 0:
+                        buildSlanderer(influence - 10);
+                        break;
+                    case 1:
+                        buildCheapMuckraker();
+                        break;
+                    case 2:
+                        buildCheapMuckraker();
+                        break;
+                    case 3:
+                        if (influence >= 300) {
+                            buildPolitician(influence - 100);
+                        } else {
+                            buildCheapPolitician();
+                        }
+                        break;
+                }
+                return;
+            }
+            if (rc.getEmpowerFactor(Constants.ALLY_TEAM, 15) >= 2 && turnsSinceSelfEmpowerer >= 11) {
+                int cost = influence / 2;
+                if (buildSelfEmpowerer(cost)) {
+                    return;
+                } else {
+                    if (buildCheapMuckraker()) return;
+                }
+            }
+            // no danger? build slanderers / big p
+            if (influence >= 150 && Math.random() < 0.2) {
+                // build politician w/ minimum 150
+                if (rc.getRoundNum() > 250 && Math.random() < 0.5) {
+                    if (buildMuckraker(Math.max(influence - 50, 150))) {
+                        return;
+                    }
+                }
+                if (buildPolitician(Math.max(influence - 50, 150))) {
+                    return;
+                }
+            }
+            boolean foundEnemyEC = !MapInfo.getKnownEnlightenmentCenterList(Constants.ENEMY_TEAM).isEmpty();
+            eS = 0;
+            eM = 0;
+            eP = 0;
+            for (RobotInfo r : Cache.ENEMY_ROBOTS) {
+                switch (r.getType()) {
+                    case SLANDERER:
+                        eS++;
+                        break;
+                    case MUCKRAKER:
+                        eM++;
+                        break;
+                    case POLITICIAN:
+                        eP++;
+                        break;
+                }
+            }
+
+            double random = Math.random();
+            if ((rc.getRoundNum() <= 2 || rc.getRoundNum() >= 30) && (slandererCount == 0 || random < 0.4) && eM == 0) {
+                if (buildSlanderer(influence)) {
+                    return;
+                }
+            }
+            random = Math.random();
+            if (random < (foundEnemyEC ? 0.4 : 0.45)) {
+                if (buildCheapPolitician()) {
+                    return;
+                }
+            }
+            // otherwise, build 1 cost muckrakers
+            buildCheapMuckraker();
+        }
+    }
+
+    public static void bid(int influence) throws GameActionException {
+        if (rc.getTeamVotes() == 751) {
+            return;
+        }
+        if (influence <= 100) {
+            return;
+        }
+        double r = Math.random();
+        int amount;
+        if (r < 0.2) {
+            amount = Math.max(Util.randBetween(1, 3), (int) (0.1 * rc.getInfluence() / (1500 - rc.getRoundNum())));
+        } else if (r < 0.4) {
+            amount = (int) (0.01 * influence);
+        } else {
+            amount = (int) (0.02 * influence);
+        }
+        rc.bid(amount);
+    }
+
+    public static int getSaveAmount() {
+        // Save if we see enemy EC or enemy politician
+        int res = LambdaUtil.arraysStreamSum(Cache.ALL_ROBOTS,
+                r -> r.getTeam() != Constants.ALLY_TEAM &&
+                        (r.getType() == RobotType.ENLIGHTENMENT_CENTER || r.getType() == RobotType.POLITICIAN),
+                r -> Math.max(r.getConviction() - Constants.POLITICIAN_EMPOWER_PENALTY + 1, 0));
+        //if(perTurnProfit >= 50) res = Math.max(res, 2 * perTurnProfit);
+        return res;
+    }
+
+    public static boolean reactBuild(int influence) {
+        // If there are enemy slanderers in sensing radius, build muckrakers
+        if (LambdaUtil.arraysAnyMatch(Cache.ENEMY_ROBOTS, r -> r.getType() == RobotType.SLANDERER)) {
+            if (buildCheapMuckraker()) return true;
+        }
+        // TODO: Loop through all communicated units to figure this out
+        boolean muckrakerNear = CentralCommunication.nearestEnemyType == RobotType.MUCKRAKER &&
+                (CentralCommunication.nearestEnemyDistanceSquared <= 64 ||
+                        CentralCommunication.nearestEnemyDistanceSquared <= 100 && Math.random() < 0.5);
+        // ignore small politicians's unless they're really close
+        boolean politicianNear = CentralCommunication.nearestEnemyType == RobotType.POLITICIAN &&
+                CentralCommunication.nearestEnemyDistanceSquared <= 25;
+        // do we have slanderers? is there danger (muckrakers)? build defender politicians
+        if ((slandererCount > 0 && muckrakerNear) || politicianNear) {
+            int cost = 5 * CentralCommunication.nearestEnemyConviction + Constants.POLITICIAN_EMPOWER_PENALTY;
+            if (influence >= cost) {
+                buildPolitician(cost);
+            } else {
+                // save for politician - build 1 cost muckraker
+                buildCheapMuckraker();
+            }
+            return true;
+        }
+        /*// find smallest neutral/enemy EC (in vision)
+        if (LambdaUtil.arraysStreamMin(Cache.ALL_ROBOTS,
+                r -> r.getTeam() != Constants.ALLY_TEAM && r.getType() == RobotType.ENLIGHTENMENT_CENTER,
+                r -> r.getConviction() + Constants.POLITICIAN_EMPOWER_PENALTY).map(cost -> {
+            if (influence >= cost) {
+                buildPolitician(cost);
+            } else {
+                buildCheapMuckraker();
+            }
+            return true;
+        }).orElse(false)) {
+            return false;
+        }
+        // find neutral EC communicated
+        if (MapInfo.getKnownEnlightenmentCenterList(Team.NEUTRAL)
+                .min(Comparator.comparingInt(x -> x.location.distanceSquaredTo(Cache.MY_LOCATION)))
+                .map(x -> {
+                    MapLocation location = x.location;
+                    int conviction = x.lastKnownConviction;
+                    int cost = conviction + Constants.POLITICIAN_EMPOWER_PENALTY;
+                    Util.println("Spawning Politician for Neutral EC: " + cost);
+                    if (influence >= cost) {
+                        buildPolitician(cost);
+                    } else {
+                        buildCheapMuckraker();
+                    }
+                    return true;
+                }).orElse(false)) {
+            return true;
+        }*/
+        return false;
+    }
+
+    public static boolean buildCheapMuckraker() {
+        int influence = Math.max(1, (int) (0.1 * rc.getInfluence() / (1500 - rc.getRoundNum())));
+        if (Util.tryBuildRobotTowards(RobotType.MUCKRAKER, Util.randomAdjacentDirection(), influence)) {
+            muckrakerCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean buildMuckraker(int influence) {
+        int cap = Math.max(1000, (int) (0.5 * rc.getInfluence() / (1500 - rc.getRoundNum())));
+        influence = Math.min(influence, cap);
+        if (Util.tryBuildRobotTowards(RobotType.MUCKRAKER, Util.randomAdjacentDirection(), influence)) {
+            muckrakerCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean buildSlanderer(int influence) {
+        // check if slanderer will provide negligible eco
+        if (rc.getConviction() >= 50000 || rc.getConviction() >= 300 * (1500 - rc.getRoundNum())) return false;
+        influence = Math.min(influence, 463);
+        Direction awayFromEnemies = enemyDirection.directionTo(Cache.MY_LOCATION);
+        if (awayFromEnemies == Direction.CENTER) {
+            awayFromEnemies = Util.randomAdjacentDirection();
+        }
+        int cost = SlandererBuild.getBuildInfluence(influence);
+        if (cost > 0 && Util.tryBuildRobotTowards(RobotType.SLANDERER, awayFromEnemies, cost)) {
+            slandererCount++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean buildCheapPolitician() {
+        int influence = Math.max(Util.randBetween(14, 20), (int) (0.1 * rc.getInfluence() / (1500 - rc.getRoundNum())));
+        if (influence % 10 == 6) influence++;
+        if (Util.tryBuildRobotTowards(RobotType.POLITICIAN, Util.randomAdjacentDirection(), influence)) {
+            politicianCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean buildPolitician(int influence) {
+        int cap = Math.max(1000, (int) (0.5 * (rc.getInfluence() / (1500 - rc.getRoundNum()))));
+        influence = Math.min(influence, cap);
+        if (influence % 10 == 6) influence++;
+        if (Util.tryBuildRobotTowards(RobotType.POLITICIAN, Util.randomAdjacentDirection(), influence)) {
+            politicianCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean buildSelfEmpowerer(int influence) {
+        influence = influence + 6 - (influence % 10);
+        if (influence <= 50) return false;
+        if (Util.tryBuildRobotTowards(RobotType.POLITICIAN, Util.randomAdjacentDirection(), influence)) {
+            politicianCount++;
+            turnsSinceSelfEmpowerer = 0;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ spiral/util/CentralCommunication.java
@@ -0,0 +1,180 @@
+package spiral.util;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.Team;
+
+public class CentralCommunication {
+    private static RobotController rc;
+    public static MapLocation nearestEnemy;
+    public static int nearestEnemyDistanceSquared = Integer.MAX_VALUE;
+    public static RobotType nearestEnemyType;
+    public static int nearestEnemyConviction = 0;
+    public static void init(RobotController rc) {
+        CentralCommunication.rc = rc;
+        registered = new BooleanArray();
+    }
+    private static BooleanArray registered;
+    // Stored as singly linked list
+    static class UnitListNode {
+        MapLocation location;
+        int id;
+        UnitListNode next;
+        public UnitListNode(MapLocation location, int id, UnitListNode next) {
+            this.location = location;
+            this.id = id;
+            this.next = next;
+        }
+    }
+    private static UnitListNode unitListHead = null;
+    private static int unitListSize = 0;
+    private static final int UNIT_LIST_MAX_SIZE = 120;
+
+    public static final int DO_NOTHING_FLAG = 0b0000000_0000000_10000_10000;
+    // 14 bits: rotate between [minX, maxX], [minY, maxY], [friendly ec], [enemy ec], [neutral ec]
+    public static final int ROTATION_SHIFT_X = 17;
+    public static final int ROTATION_SHIFT_Y = 10;
+    public static final int ROTATION_MASK = 0b0111_1111;
+    public static final int ROTATION_OFFSET = 64;
+    // 10 bits: broadcast nearest known enemy to this EC
+    public static void loop() throws GameActionException {
+        rc.setFlag(0); // in case we run out of bytecodes
+        // find nearest enemy
+        RobotInfo enemy = Util.getClosestEnemyRobot();
+        if (enemy == null) {
+            nearestEnemy = null;
+            nearestEnemyDistanceSquared = Integer.MAX_VALUE;
+            nearestEnemyType = null;
+            nearestEnemyConviction = 0;
+        } else {
+            nearestEnemy = enemy.getLocation();
+            nearestEnemyDistanceSquared = Cache.MY_LOCATION.distanceSquaredTo(nearestEnemy);
+            nearestEnemyType = enemy.getType();
+            nearestEnemyConviction = enemy.getInfluence();
+        }
+        // read flags of each known robot (delete from list if not alive)
+        UnitListNode prev = null;
+        UnitListNode current = unitListHead;
+        while (current != null) {
+            int id = current.id;
+            if (rc.canGetFlag(id)) {
+                int flag = rc.getFlag(id) ^ UnitCommunication.DO_NOTHING_FLAG;
+                // process info
+                int dx = ((flag >> UnitCommunication.CURRENT_UNIT_OFFSET_X_SHIFT) & UnitCommunication.CURRENT_UNIT_OFFSET_MASK) - UnitCommunication.OFFSET_SHIFT;
+                int dy = ((flag >> UnitCommunication.CURRENT_UNIT_OFFSET_Y_SHIFT) & UnitCommunication.CURRENT_UNIT_OFFSET_MASK) - UnitCommunication.OFFSET_SHIFT;
+                if (dx == 0 && dy == 0) {
+                    // TODO: communicate about map edge?
+                } else {
+                    MapLocation specifiedLocation = current.location.translate(dx, dy);
+                    RobotType type = RobotType.values()[(flag >> UnitCommunication.CURRENT_UNIT_TYPE_SHIFT) & UnitCommunication.CURRENT_UNIT_TYPE_MASK];
+                    if (type == RobotType.SLANDERER) {
+                        if (!MapInfo.enemySlandererLocations.contains(specifiedLocation)) {
+                            MapInfo.enemySlandererLocations.add(specifiedLocation, Cache.TURN_COUNT);
+                        }
+                    }
+                    if (type == RobotType.ENLIGHTENMENT_CENTER) {
+                        int teamOrdinal = (flag >> UnitCommunication.CURRENT_EC_TEAM_SHIFT) & UnitCommunication.CURRENT_EC_TEAM_MASK;
+                        int conviction = (flag & UnitCommunication.CURRENT_EC_CONVICTION_MASK) * 2;
+                        Team ecTeam = Team.values()[teamOrdinal];
+                        if (!specifiedLocation.equals(Cache.MY_LOCATION)) {
+                            MapInfo.addKnownEnlightenmentCenter(ecTeam, specifiedLocation, conviction);
+                        }
+                    } else {
+                        int info = flag & UnitCommunication.CURRENT_UNIT_INFO_MASK;
+                        int distanceSquared = Cache.MY_LOCATION.distanceSquaredTo(specifiedLocation);
+                        if (distanceSquared < nearestEnemyDistanceSquared) {
+                            nearestEnemyDistanceSquared = distanceSquared;
+                            nearestEnemy = specifiedLocation;
+                            nearestEnemyType = type;
+                            nearestEnemyConviction = info;
+                        }
+                    }
+                }
+                // update location
+                Direction dir = Direction.values()[Math.min(8, flag >> UnitCommunication.CURRENT_DIRECTION_SHIFT)];
+                current.location = current.location.add(dir);
+                prev = current; // prev is shifted one over
+            } else {
+                if (prev == null) {
+                    unitListHead = current.next;
+                    // prev remains null
+                } else {
+                    prev.next = current.next;
+                    // prev remains the same
+                }
+                unitListSize--;
+            }
+            current = current.next;
+        }
+        // register any new ally robots (either though vision or building)
+        if (unitListSize < UNIT_LIST_MAX_SIZE) {
+            for (RobotInfo ally : Cache.ALLY_ROBOTS) {
+                // some sort of set - allocate in chunks of 4096?
+                if (ally.getType() != RobotType.ENLIGHTENMENT_CENTER) {
+                    int id = ally.getID();
+                    if (!registered.getAndSetTrue(id - Constants.MIN_ROBOT_ID)) {
+                        // register in singly linked list
+                        unitListHead = new UnitListNode(ally.getLocation(), id, unitListHead);
+                        unitListSize++;
+                        if (unitListSize >= UNIT_LIST_MAX_SIZE) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static final int NEAREST_ENEMY_OFFSET = 16;
+    public static final int NEAREST_ENEMY_X_SHIFT = 5;
+    public static final int NEAREST_ENEMY_MASK = 0b1_1111;
+    public static void postLoop() throws GameActionException {
+        // set flag
+        int flag = 0;
+        if (nearestEnemy == null) {
+            // set dx, dy to be (0, 0) to signal that there are no nearestEnemies
+            nearestEnemy = Cache.MY_LOCATION;
+            Util.setIndicatorDot(Cache.MY_LOCATION, 255, 255, 0); // yellow
+        } else {
+            Util.setIndicatorDot(nearestEnemy, 0, 255, 0); // green
+        }
+        // 5 bits on relative x location and relative y location
+        int dx = nearestEnemy.x - Cache.MY_LOCATION.x + NEAREST_ENEMY_OFFSET;
+        int dy = nearestEnemy.y - Cache.MY_LOCATION.y + NEAREST_ENEMY_OFFSET;
+        if (dx < 0 || dx > NEAREST_ENEMY_MASK || dy < 0 || dy > NEAREST_ENEMY_MASK) {
+            // underflow/overflow: set to current location to mark no enemy nearby
+            dx = NEAREST_ENEMY_OFFSET;
+            dy = NEAREST_ENEMY_OFFSET;
+            Util.setIndicatorDot(Cache.MY_LOCATION, 255, 128, 0); // orange
+        }
+        flag = flag | (dx << NEAREST_ENEMY_X_SHIFT) | dy;
+        // 7 bits on relative x location and relative y location
+        MapLocation rotationLocation = null;
+        switch (Cache.TURN_COUNT % 5) {
+            case 0: // heartbeat
+                rotationLocation = Cache.MY_LOCATION;
+                break;
+            case 1: // [ally ec]
+                rotationLocation = MapInfo.getKnownEnlightenmentCenterList(Constants.ALLY_TEAM).getRandomLocation().orElse(null);
+                break;
+            case 2: // [enemy ec]
+                rotationLocation = MapInfo.getKnownEnlightenmentCenterList(Constants.ENEMY_TEAM).getRandomLocation().orElse(null);
+                break;
+            case 3: // [neutral ec]
+                rotationLocation = MapInfo.getKnownEnlightenmentCenterList(Team.NEUTRAL).getRandomLocation().orElse(null);
+                break;
+            case 4: // [enemy slanderers]
+                rotationLocation = MapInfo.enemySlandererLocations.getRandomLocation().orElse(null);
+                break;
+        }
+        if (rotationLocation != null) {
+            int rotationDx = rotationLocation.x - Cache.MY_LOCATION.x + ROTATION_OFFSET;
+            int rotationDy = rotationLocation.y - Cache.MY_LOCATION.y + ROTATION_OFFSET;
+            flag = flag | (rotationDx << ROTATION_SHIFT_X) | (rotationDy << ROTATION_SHIFT_Y);
+        }
+        rc.setFlag(flag ^ DO_NOTHING_FLAG);
+    }
+}
